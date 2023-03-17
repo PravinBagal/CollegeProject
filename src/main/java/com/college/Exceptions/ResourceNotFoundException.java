071b666c25ab0c7da785ac9d6d0c059d87527531
Pravin S.Bagal <101473516+PravinBagal@users.noreply.github.com>
2023-03-17 16:16:35 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/college/Exceptions/ResourceNotFoundException.java
+++ src/main/java/com/college/Exceptions/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package com.college.Exceptions;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception {
     public ResourceNotFoundException(String msg)
     {
